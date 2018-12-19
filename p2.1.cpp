e0f896e1247873cc3c24a8778c919fa6bd5ef076
Susmit <susmit.py@gmail.com>
2018-12-19 23:37:57 +0530

    Complex prac number

--- p2.1.cpp
+++ p2.1.cpp
@@ -3,12 +3,6 @@
 
 using namespace std;
 
-struct C
-{
-  int real;
-  int imag;
-};
-
 class Complex
 {
 public:
@@ -17,26 +11,31 @@ public:
     real = a;
     imag = b;
   }
-  friend struct C add(Complex, Complex);
-private:
-  int real, imag;
+  friend Complex add(Complex, Complex);
+
+  int real;
+  int imag;
 };
 
-struct C add(Complex c1, Complex c2)
+Complex add(Complex a, Complex b)
 {
-  int r, c;
-  r = c1.real + c2.real;
-  c = c1.imag + c2.imag;
-  struct C ans;
-  ans.real = r;
-  ans.imag = c;
-  return ans;
+
+  return Complex(a.real + b.real, a.imag + b.imag);
 }
 
 int main()
 {
-  Complex c1(5,3), c2(5,3);
-  cout << add(c1,c2).real << endl;
-  cout << add(c1,c2).imag << endl;
+  int c1r, c1i, c2r, c2i;
+  cout << "Enter real and imag of c1 and c2 each on a new line" << endl;
+  cin >> c1r;
+  cin >> c1i;
+  cin >> c2r;
+  cin >> c2i;
+  Complex c1(c1r, c1i);
+  Complex c2(c2r, c2i);
+  cout << "c1: " << c1.real << " " << c1.imag << endl;
+  cout << "c2: " << c2.real << " " << c2.imag << endl;
+  Complex c3 = add(c1,c2);
+  cout << "Added:\n" << "\tReal: " << c3.real << "\n\t" << "Imag: " << c3.imag << endl;
   return 0;
 }
