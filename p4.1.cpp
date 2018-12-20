7be716731bdbce300a13f6a025a0699c07bd07df
Susmit <susmit.py@gmail.com>
2018-12-20 21:23:17 +0530

    toggle

---
+++ p4.1.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <string>
+#include <cctype>
+
+using namespace std;
+
+class ToggleString
+{
+public:
+  ToggleString(char s[25])
+  {
+    setStr(s);
+  }
+  char* toggle()
+  {
+    char str[25];
+    strcpy(str, readStr());
+    int l = strlen(str);
+    for (int i = 0; i < l; i++)
+    {
+      if (islower(str[i]))
+      {
+        str[i] = toupper(str[i]);
+      }
+      else
+      {
+        str[i] = tolower(str[i]);
+      }
+    }
+    return str;
+  }
+  char* display()
+  {
+    return readStr();
+  }
+
+  void operator - ()
+  {
+    setStr(toggle());
+  }
+private:
+  char str[25];
+  char* readStr()
+  {
+    return str;
+  }
+  void setStr(char s[])
+  {
+    strcpy(str, s);
+  }
+
+};
+
+int main()
+{
+  char s[25];
+  cout << "Enter the string to toggle case: ";
+  cin >> s;
+  ToggleString TS(s);
+  -TS;
+  cout << "After toggling: " << TS.display() << endl;
+  return 0;
+}
